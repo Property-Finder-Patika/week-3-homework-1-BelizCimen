8b0b608407e9d245c977fb7578d62d8c55cfbe3f
Beliz Çimen Sevinç <belizcimensevinc@gmail.com>
2022-07-15 23:05:47 +0300

    prime factorization

---
+++ prime-factorization/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func PrimeFactors(n int) (pfs []int) {
+	// Get the number of 2s that divide n
+	for n%2 == 0 {
+		pfs = append(pfs, 2)
+		n = n / 2
+	}
+
+	for i := 3; i*i <= n; i = i + 2 {
+		// while i divides n, append i and divide n
+		for n%i == 0 {
+			pfs = append(pfs, i)
+			n = n / i
+		}
+	}
+
+	if n > 2 {
+		pfs = append(pfs, n)
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println("Enter the number ")
+	var n int
+	fmt.Scanln(&n)
+	fmt.Println(PrimeFactors(n))
+}
